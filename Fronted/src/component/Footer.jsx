38f2156f13ca62f537e295bb78e84a7a785ae052
sunnyshah011 <sunnyshah35350@gmail.com>
2025-08-16 07:50:28 +0545

    clone

--- Fronted/src/component/Footer.jsx
+++ Fronted/src/component/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div className='h-15 w-full p-3 flex justify-center items-center bg-gray-300'>
       <div className='font-semibold text-2xl'>
-        PRIVACY AND POLICY
+        PRIVACY & POLICY
       </div>
     </div>
   )
