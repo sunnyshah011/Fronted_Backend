4beda544aa397ec7d859373e9d8c2c6b47503763
sunnyshah011 <sunnyshah35350@gmail.com>
2025-08-18 16:23:05 +0545

    admin panel

--- Backend/routes/product.route.js
+++ Backend/routes/product.route.js
@@ -17,28 +17,3 @@ productRouter.get('/list', listProduct)
 
 
 export default productRouter
-
-
-
-// import express from 'express'
-// import { addProduct, listProduct, removeProduct, singleProduct } from '../controllers/product.controller.js'
-// import upload from '../middleware/multer.middleware.js'
-
-// const productRouter = express.Router()
-
-// productRouter.post(
-//     '/add',
-//     upload.fields([
-//         { name: 'image1', maxCount: 1 },
-//         { name: 'image2', maxCount: 1 },
-//         { name: 'image3', maxCount: 1 },
-//         { name: 'image4', maxCount: 1 }
-//     ]),
-//     addProduct
-// )
-
-// productRouter.get('/list', listProduct)
-// productRouter.post('/remove', removeProduct)
-// productRouter.post('/single', singleProduct)
-
-// export default productRouter
--- Fronted/vite.config.js
+++ Fronted/vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/',
-   base: '/React_Project/',
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       poppins: ['Poppins', 'sans-serif'],
-  //     },
-  //   },
-  // },
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
-})
+  base: "/React_Project/",
+
+  plugins: [tailwindcss(), react()],
+
+  server: { port: 5173 },
+});
+// theme: {
+//   extend: {
+//     fontFamily: {
+//       poppins: ['Poppins', 'sans-serif'],
+//     },
+//   },
+// },
