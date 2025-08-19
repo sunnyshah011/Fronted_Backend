f631c9fee802336aec8815efd935ebb0f8240461
sunnyshah011 <sunnyshah35350@gmail.com>
2025-08-19 23:45:05 +0545

    admin

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
 import userRouter from "./routes/user.route.js";
 import productRouter from "./routes/product.route.js";
+import bodyParser from 'body-parser'
 
 //App Config
 const app = express();
@@ -15,10 +16,12 @@ connectCloudinary();
 //middlewares
 app.use(express.json());
 app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 //api endpoints
 app.use('/api/user', userRouter)
-app.use('/api/product',productRouter)
+app.use('/api/product', productRouter)
 
 app.get("/", (req, res) => {
   res.send("API");
@@ -28,31 +31,3 @@ app.listen(port, () => {
   console.log("server started on :: ", port);
 });
 
-
-
-// import express from 'express'
-// import mongoose from 'mongoose'
-// import productRouter from './routes/product.routes.js'
-// import cors from 'cors'
-// import bodyParser from 'body-parser'
-
-// const app = express()
-
-// // Middleware
-// app.use(cors())
-// app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
-
-// // Serve uploaded files statically
-// app.use('/uploads', express.static('uploads'))
-
-// // Routes
-// app.use('/api/product', productRouter)
-
-// // DB and Server
-// mongoose.connect('mongodb://localhost:27017/myshop')
-//   .then(() => {
-//     console.log("MongoDB connected")
-//     app.listen(5000, () => console.log("Server running on http://localhost:5000"))
-//   })
-//   .catch(err => console.error("MongoDB error:", err))
