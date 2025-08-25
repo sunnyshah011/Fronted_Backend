1496121daad4fd430b0cd8b9aa6954111be6ee7c
sunnyshah011 <sunnyshah35350@gmail.com>
2025-08-25 18:30:20 +0545

    afs

--- Backend/models/product.model.js
+++ Backend/models/product.model.js
@@ -1,17 +1,36 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  description: { type: String, required: true },
-  price: { type: Number, required: true },
-  image: { type: Array, required: true },
-  category: { type: String, required: true },
-  subCategory: { type: String, required: true },
-  sizes: { type: Array, required: true },
-  date: { type: Number, required: true },
-});
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: Array, required: true },
+    category: { type: String, required: true },
+    subCategory: { type: String, required: true },
+    sizes: { type: Array, required: true },
+    date: { type: Number, required: true }
+})
 
-const productModel =
-  mongoose.models.product || mongoose.model("product", productSchema);
+const productModel = mongoose.models.product || mongoose.model("product", productSchema)
 
-export default productModel;
+export default productModel
+
+
+
+// import mongoose from "mongoose";
+
+// const productSchema = new mongoose.Schema({
+//   name: { type: String, required: true },
+//   description: { type: String, required: true },
+//   price: { type: Number, required: true },
+//   image: { type: Array, required: true }, // Array of filenames
+//   category: { type: String, required: true },
+//   subCategory: { type: String, required: true },
+//   sizes: { type: Array, required: true },
+//   date: { type: Number, default: Date.now },
+// });
+
+// const productModel =
+//   mongoose.models.product || mongoose.model("product", productSchema);
+
+// export default productModel;
